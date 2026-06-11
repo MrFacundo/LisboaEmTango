2170773cc200ed15e29a7aebabef5ac1b35b092d
Facundo Troitero <facundo.troitero@criticalsoftware.com>
2026-06-11 10:58:02 +0100

    update form to use netlify

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,10 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { useState } from "react";
-import { useFormspark } from "@formspark/use-formspark";
 import tw from "twin.macro";
 import { FormContainer, Input, Label, SubmitButton } from "../globalStyles";
-
-const formsparkFormId = process.env.REACT_APP_FORMSPARK_FORM_ID;
 
 const TextArea = tw.textarea`
 	rounded-sm
@@ -15,84 +12,110 @@ const TextArea = tw.textarea`
 `;
 
 const ContactForm = () => {
-	const { t } = useTranslation();
+    const { t } = useTranslation();
 
-	const [submit, submitting] = useFormspark({
-		formId: formsparkFormId,
-	});
+    const initialFormState = {
+        email: "",
+        name: "",
+        message: "",
+    };
 
-	const initialFormState = {
-		email: "",
-		name: "",
-		message: "",
-	};
+    const [formData, setFormData] = useState(initialFormState);
 
-	const [formData, setFormData] = useState(initialFormState);
+    const [notification, setNotification] = useState();
 
-	const [notification, setNotification] = useState();
+    const onSubmit = (e) => {
+        e.preventDefault();
 
-	const onSubmit = async (e) => {
-		e.preventDefault();
-		await submit({ ...formData });
-		setFormData(initialFormState);
-		setNotification({
-			text: "Thanks, someone will be in touch shortly.",
-		});
-	};
+        const form = e.target;
 
-	return (
-		<FormContainer>
-			<form className="shadow-none" onSubmit={onSubmit}>
-				<div className="flex flex-col">
-					<Label htmlFor="name">{t("form.name")}</Label>
-					<Input
-						value={formData.name}
-						onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-						type="text"
-						id="name"
-						required
-						maxLength="30"
-					/>
-				</div>
-				<div className="flex flex-col">
-					<Label htmlFor="email">{t("form.email")}</Label>
-					<Input
-						value={formData.email}
-						onChange={(e) =>
-							setFormData({ ...formData, email: e.target.value })
-						}
-						id="email"
-						type="email"
-						required
-						pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-					/>
-				</div>
-				<div className="flex flex-col">
-					<Label htmlFor="message">{t("form.message")}</Label>
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams({
+                "form-name": "contact",
+                ...formData,
+            }).toString(),
+        })
+            .then(() => {
+                setFormData(initialFormState);
+                setNotification({
+                    text: "Thanks, someone will be in touch shortly.",
+                });
+            })
+            .catch(console.error);
+    };
 
-					<TextArea
-						value={formData.message}
-						onChange={(e) =>
-							setFormData({ ...formData, message: e.target.value })
-						}
-						type="message"
-						id="message"
-						required
-						maxLength="100"
-						rows={4}
-					/>
-				</div>
-				<SubmitButton type="submit" disabled={submitting}>
-					{submitting ? t("form.submitting") : t("form.submit")}
-				</SubmitButton>
-				{notification && (
-					<div className={`my-2 w-full p-4 bg-green-500"`}>
-						{notification.text}
-					</div>
-				)}
-			</form>
-		</FormContainer>
-	);
+    return (
+        <FormContainer>
+            <form
+                name="contact"
+                data-netlify="true"
+                netlify-honeypot="bot-field"
+                onSubmit={onSubmit}
+                className="shadow-none"
+            >
+                <input type="hidden" name="form-name" value="contact" />
+                <input type="hidden" name="bot-field" />
+
+                <div className="flex flex-col">
+                    <Label htmlFor="name">{t("form.name")}</Label>
+                    <Input
+                        name="name"
+                        value={formData.name}
+                        onChange={(e) =>
+                            setFormData({ ...formData, name: e.target.value })
+                        }
+                        type="text"
+                        id="name"
+                        required
+                        maxLength="30"
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <Label htmlFor="email">{t("form.email")}</Label>
+                    <Input
+                        name="email"
+                        value={formData.email}
+                        onChange={(e) =>
+                            setFormData({ ...formData, email: e.target.value })
+                        }
+                        id="email"
+                        type="email"
+                        required
+                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <Label htmlFor="message">{t("form.message")}</Label>
+
+                    <TextArea
+                        name="message"
+                        value={formData.message}
+                        onChange={(e) =>
+                            setFormData({ ...formData, message: e.target.value })
+                        }
+                        id="message"
+                        required
+                        maxLength="100"
+                        rows={4}
+                    />
+                </div>
+
+                <SubmitButton type="submit">
+                    {t("form.submit")}
+                </SubmitButton>
+
+                {notification && (
+                    <div className="my-2 w-full p-4 bg-green-500">
+                        {notification.text}
+                    </div>
+                )}
+            </form>
+        </FormContainer>
+    );
 };
 
 export default ContactForm;
